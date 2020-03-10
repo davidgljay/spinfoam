58cf9ae34873346f8b5ff961d7b03c83fbfc2dee
David Jay <davidgljay@gmail.com>
2020-03-10 11:22:40 -0700

    Switching to docs for github pages.

---
+++ docs/precache-manifest.4bb1dd456bbd90098c816089bcf04660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912f67191b36715aa841021439430b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc2eedd76ded7847fc0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "405f385222e19ed82104",
+    "url": "/static/js/2.2355c958.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2355c958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc2eedd76ded7847fc0",
+    "url": "/static/js/main.1b5fb518.chunk.js"
+  },
+  {
+    "revision": "89f79bec79c0e5b8c148",
+    "url": "/static/js/runtime-main.3aed1114.js"
+  }
+]);
