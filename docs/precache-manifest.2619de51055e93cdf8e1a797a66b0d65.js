5af348f52ca46ca722d24816fd55b9771226a014
David Jay <davidgljay@gmail.com>
2020-03-29 16:32:21 -0700

    Handling build effectively.

---
+++ docs/precache-manifest.2619de51055e93cdf8e1a797a66b0d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ca6ba1ac0bb7b1238fe6e04c19912b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "432cc76966795eaa98a9",
+    "url": "./static/css/main.433565f3.chunk.css"
+  },
+  {
+    "revision": "f1392de9fa9905fcab59",
+    "url": "./static/js/2.cec3a7ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.cec3a7ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432cc76966795eaa98a9",
+    "url": "./static/js/main.236a85b8.chunk.js"
+  },
+  {
+    "revision": "4eb6d52bd96ee3dfd155",
+    "url": "./static/js/runtime-main.5746f5a7.js"
+  }
+]);
